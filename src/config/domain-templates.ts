8c5b27eb8bacd71f96d242620d5ce9a2e839c6ff
xciphertv <ciphertv@proton.me>
2025-07-21 00:39:53 -0500

    Update domain-templates.ts

--- src/config/domain-templates.ts
+++ src/config/domain-templates.ts
@@ -76,6 +76,61 @@ export const domainTemplates: DomainTemplate[] = [
   },
 
   {
+    pattern: /\.megaup\.cc/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url: URL) => {
+      return {
+        'origin': 'https://megaup.cc',
+        'referer': 'https://megaup.cc/',
+      };
+    }
+  },
+
+  {
+    pattern: /\.nebulax-89\.biz/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url: URL) => {
+      return {
+        'origin': 'https://megaup.cc',
+        'referer': 'https://megaup.cc/',
+      };
+    }
+  },
+
+    {
+    pattern: /\.odyssey-19\.biz/i,
+    headers: {
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
+      'accept': '*/*',
+      'accept-language': 'en-US,en;q=0.5',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+    },
+    headersFn: (url: URL) => {
+      return {
+        'origin': 'https://megaup.cc',
+        'referer': 'https://megaup.cc/',
+      };
+    }
+  },
+  
+  
+  {
     pattern: /\.anih1\.top$/i,
     headers: {
       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:137.0) Gecko/20100101 Firefox/137.0',
